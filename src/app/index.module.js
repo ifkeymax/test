c79f66ef34b79e338b079bcb74d41cb1d9a7bf5d
ifkeymax <ifkeymax@gmail.com>
2017-01-21 01:38:20 +0200

    .init frontend

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('test', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngRoute', 'ngMaterial', 'toastr']);
+
+})();
